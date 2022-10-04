6a1efe8cdc0b5a6653d7235b87747c6e1d3c12c3
Debo <mail@deboder.nl>
2022-10-04 05:01:27 +0200

    day05start

---
+++ src/aoc/y2015/Day05.java
@@ -0,0 +1,31 @@
+package aoc.y2015;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+
+import aoc.AU;
+
+public class Day05 extends AU {
+
+    public static void main(String[] args) {
+//        var input = getInputAsString("src/aoc/y2021/input/day");
+        var input = getInputAsStream("src/aoc/y2021/input/day")
+                .toList();
+
+        solveQ1(input);
+//        solveQ2(input);
+
+    }
+
+    static void solveQ1(List<String> input) {
+        println("Day x Q1: " + "");
+    }
+}
+
