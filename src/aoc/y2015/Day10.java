4ebda13d3b12a2c458c44d7b62aa3ec706a389d9
Debo <mail@deboder.nl>
2022-10-05 17:36:46 +0200

    2015 day10

--- src/aoc/y2015/Day10.java
+++ src/aoc/y2015/Day10.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class Day10 extends AU {
     private static String day = "10";
+
     public static void main(String[] args) {
 
         solveQ1(null);
@@ -18,10 +19,33 @@ public class Day10 extends AU {
 //    }
 
     static void solveQ1(List<String> input) {
-        var result = 0;
-
-
-        println("Day " + day +" Q1: " + result);
+//        String str = "1";
+        String str = "1113122113";
+        for (int x = 0; x < 50; x++) {
+            str = expand(str);
+        }
+        println("Day " + day + " Q1: " + str.length());
     }
+
+
+    static String expand(String string) {
+        var sb = new StringBuilder();
+        char current = string.charAt(0);
+        int count = 1;
+        for (int i = 1; i < string.length(); i++) {
+            char tmp = string.charAt(i);
+            if (tmp == current) {
+                count++;
+            } else {
+                sb.append(count).append(current);
+                current = tmp;
+                count = 1;
+            }
+        }
+        sb.append(count).append(current);
+        return sb.toString();
+    }
+
+    ;
 }
 
