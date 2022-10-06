c4675a7b4cb54e5e937fa8724cca2fb61ab030ca
Debo <mail@deboder.nl>
2022-10-06 17:13:23 +0200

    day15start

---
+++ src/aoc/y2015/Day15.java
@@ -0,0 +1,31 @@
+package aoc.y2015;
+
+import aoc.AU;
+
+import java.util.stream.Collectors;
+
+public class Day15 extends AU {
+
+    public static void main(String[] args) {
+        var day = new Day15();
+        println("Day " + day.getDay() + " Q1: " + day.solveQ1());
+        day = new Day15();
+        println("Day " + day.getDay() + " Q2: " + day.solveQ2());
+    }
+
+    Object solveQ2() {
+        return null;
+    }
+
+    Object solveQ1() {
+        var input = getInputLinesT();
+        var result = 0;
+
+        return result;
+    }
+
+
+    public String getDay() { return "15";};
+
+}
+
