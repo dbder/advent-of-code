967f893b126793f50183b9433c6daeefb4f09c76
Debo <mail@deboder.nl>
2022-10-06 13:12:03 +0200

    refactor

--- src/aoc/y2015/Day14.java
+++ src/aoc/y2015/Day14.java
@@ -4,10 +4,21 @@ import aoc.AU;
 import aoc.AocException;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Day14 extends AU {
+
+    List<Counter> counters = Arrays.asList(
+            new Counter("V", 19, 7, 124),
+            new Counter("Rudolph", 3, 15, 28),
+            new Counter("Donner", 19, 9, 164),
+            new Counter("Blitzen", 19, 9, 158),
+            new Counter("Comet", 13, 7, 82),
+            new Counter("Cupid", 25, 6, 145),
+            new Counter("Dasher", 14, 3, 38),
+            new Counter("Dancer", 3, 16, 37),
+            new Counter("Prancer", 25, 6, 143)
+    );
 
     public static void main(String[] args) {
         new Day14().solveQ1();
@@ -15,90 +26,63 @@ public class Day14 extends AU {
     }
 
     void solveQ2() {
-//        println("Day " + day +" Q2: " + "");
-    }
-
-    void solveQ1() {
-        var input = getInputLinesT();
-        var result = 0;
-
-        List<Counter> counters = Arrays.asList(
-                new Counter("V", 19, 7, 124),
-                new Counter("Rudolph", 3, 15, 28),
-                new Counter("Donner", 19, 9, 164),
-                new Counter("Blitzen", 19, 9, 158),
-                new Counter("Comet", 13, 7, 82),
-                new Counter("Cupid", 25, 6, 145),
-                new Counter("Dasher", 14, 3, 38),
-                new Counter("Dancer", 3, 16, 37),
-                new Counter("Prancer", 25, 6, 143)
-        );
-
         for (int i = 0; i < 2503; i++) {
             for (Counter counter : counters) {
-                counter.move(10000000);
+                counter.move();
             }
-            Collections.sort(counters, (a, b) -> b.distance - a.distance);
-            counters.stream().filter(c -> c.distance == counters.get(0).distance).forEach(c -> c.score++);
+            counters.sort((a, b) -> b.distance - a.distance);
+            counters.stream()
+                    .filter(c -> c.distance == counters.get(0).distance)
+                    .forEach(c -> c.score++);
         }
+        println("Day " + getDay() + " Q2: " + counters.stream().mapToInt(i -> i.score).max());    }
 
-        counters.forEach(System.out::println);
-
-        result = 0;
-        for (Counter counter : counters) {
-            result = Math.max(result, counter.score);
+    void solveQ1() {
+        for (int i = 0; i < 2503; i++) {
+            for (Counter counter : counters) {
+                counter.move();
+            }
         }
-
-
-        println("Day " + getDay() + " Q1: " + result);
+        println("Day " + getDay() + " Q1: " + counters.stream().mapToInt(i -> i.distance).max());
     }
 
-
+    @Override
     public String getDay() {
         return "14";
     }
-
-    ;
-
 }
 
 class Counter {
 
     public Counter(String name, int speed, int stamina, int cooldown) {
-        this.reload = stamina;
+        this.stamina = stamina;
         this.cooldown = cooldown;
         this.name = name;
         this.speed = speed;
         currentFlyLeft = stamina;
     }
 
-    int reload;
+    int stamina;
     int cooldown;
     int currentFlyLeft;
     int currentRestLeft;
     int distance;
     String name;
     int speed;
-
     int score;
 
-    boolean move(int target) {
+    void move() {
         if (currentRestLeft > 0) {
             currentRestLeft--;
             if (currentRestLeft == 0) {
-                currentFlyLeft = reload;
+                currentFlyLeft = stamina;
             }
-            return false;
         } else if (currentFlyLeft > 0) {
             currentFlyLeft--;
             distance += speed;
-            if (distance >= target) {
-                return true;
-            }
             if (currentFlyLeft == 0) {
                 currentRestLeft = cooldown;
             }
-            return false;
         }
         throw new AocException("Should not happen");
     }
