d54b251542d5331a30f1b68b658217b23cd48863
Debo <mail@deboder.nl>
2022-10-07 17:27:55 +0200

    2015 day18end

--- src/aoc/y2015/Day18.java
+++ src/aoc/y2015/Day18.java
@@ -2,6 +2,8 @@ package aoc.y2015;
 
 import aoc.AU;
 
+import java.util.Arrays;
+
 public class Day18 extends AU {
 
     public static void main(String[] args) {
@@ -19,9 +21,83 @@ public class Day18 extends AU {
 
     Object solveQ1() {
         var input = getInputLines();
+
+        var grid = input.stream().map(s -> s.chars().mapToObj(c -> c == '#').toArray(Boolean[]::new)).toArray(Boolean[][]::new);
+
+
+        for (int i = 0; i < 100; i++) {
+            grid = step(grid);
+        }
+
+        int count = 0;
+
+        for (var row : grid) {
+            for (var cell : row) {
+                if (cell) {
+                    count++;
+                }
+            }
+        }
+
         var result = 0;
 
-        return result;
+        return count;
+    }
+
+    private Boolean[][] step(Boolean[][] grid) {
+
+        var newGrid = new Boolean[grid.length][grid[0].length];
+        newGrid[0][0] = true;
+        newGrid[0][newGrid[0].length - 1] = true;
+        newGrid[newGrid.length - 1][0] = true;
+        newGrid[newGrid.length - 1][newGrid[0].length - 1] = true;
+        grid[0][0] = true;
+        grid[0][newGrid[0].length - 1] = true;
+        grid[newGrid.length - 1][0] = true;
+        grid[newGrid.length - 1][newGrid[0].length - 1] = true;
+
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                var on = isOn(grid, x, y);
+                var neighbours = countNeighbours(grid, x, y);
+
+                if (on) {
+                    newGrid[y][x] = neighbours == 2 || neighbours == 3;
+                } else {
+                    newGrid[y][x] = neighbours == 3;
+                }
+            }
+        }
+        newGrid[0][0] = true;
+        newGrid[0][newGrid[0].length - 1] = true;
+        newGrid[newGrid.length - 1][0] = true;
+        newGrid[newGrid.length - 1][newGrid[0].length - 1] = true;
+        return newGrid;
+    }
+
+    private int countNeighbours(Boolean[][] grid, int x, int y) {
+        var count = 0;
+
+        for (int[] tp : TPS8) {
+            if (isOn(grid, x + tp[0], y + tp[1])) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    boolean isOn(Boolean[][] grid, int x, int y) {
+        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y];
+    }
+
+    void printGrid(Boolean[][] grid) {
+        for (var row : grid) {
+            for (var cell : row) {
+                print(cell ? '#' : '.');
+            }
+            println("");
+        }
     }
 
 
