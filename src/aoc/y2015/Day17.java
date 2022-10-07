67c36da4256e59f94d4d764aa86fa72a5f6c4d57
Debo <mail@deboder.nl>
2022-10-07 14:19:12 +0200

    2015 day17

--- src/aoc/y2015/Day17.java
+++ src/aoc/y2015/Day17.java
@@ -13,6 +13,12 @@ public class Day17 extends AU {
     Day17() {
         println("Day " + getDay() + " Q1: " + solveQ1());
         println("Day " + getDay() + " Q2: " + solveQ2());
+
+        for (int i = 0; i < 100; i++) {
+            if (counts[i] > 0) {
+                println(i + " " + counts[i]);
+            }
+        }
     }
 
     Object solveQ2() {
@@ -22,21 +28,23 @@ public class Day17 extends AU {
     Object solveQ1() {
         var input = getInputLines().stream().map(i -> Integer.parseInt(i)).sorted().toList();
         for (int x = 0; x < input.size(); x++) {
-            count(input, x, input.get(x), 150);
+            count(input, x, input.get(x), 150, 1);
         }
 
 
         return count;
     }
     int count = 0;
-    void count (List<Integer> input, int index, int sum, int target) {
+    int[] counts = new int[100];
+    void count (List<Integer> input, int index, int sum, int target, int containers) {
 
         if(sum == target) {
             count++;
+            counts[containers]++;
             return;
         }
         for (int i = index + 1; i < input.size(); i++) {
-            count(input, i, sum + input.get(i), target);
+            count(input, i, sum + input.get(i), target, containers + 1);
         }
     }
 
