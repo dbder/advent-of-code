915fc7d610678e55da2811cda076d826f5da7c95
Debo <mail@deboder.nl>
2022-11-30 07:25:32 +0100

    refactor

---
+++ src/main/java/aoc/y2022/Day01.java
@@ -0,0 +1,36 @@
+package aoc.y2022;
+
+import aoc.AU;
+
+public class Day01 extends AU {
+
+    @Override
+    protected String getDay() {
+        return "01";
+    }
+    public static void main(String[] args) {
+        new Day01();
+    }
+
+    Day01() {
+        println("Day " + getDay() + " Q1: " + solveQ1());
+        println("Day " + getDay() + " Q2: " + solveQ2());
+    }
+
+    Object solveQ2() {
+        var result = 0;
+        var input = getInputLines();
+        
+        return result;
+    }
+
+    Object solveQ1() {
+        var result = 0;
+        var input = getInputLines();
+
+
+        return result;
+    }
+
+}
+
