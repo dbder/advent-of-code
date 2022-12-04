6b63fe36c5bbc78b251b2861f16f0fee96c738c5
Debo <mail@deboder.nl>
2022-12-04 19:25:00 +0100

    2018 day06 start

---
+++ src/main/java/aoc/y2018/Day06.java
@@ -0,0 +1,43 @@
+package aoc.y2018;
+
+import aoc.AU;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class Day06 extends AU {
+
+    public static void main(String[] args) {
+        new Day06();
+    }
+
+    List<String> testData1 = """
+            """.lines().collect(toList());
+    List<String> testData2 = """
+            """.lines().collect(toList());
+
+    Day06() {
+        if (!testData1.isEmpty()) println("Test Q1: " + solveQ1(testData1));
+        if (!testData2.isEmpty()) println("Test Q1: " + solveQ1(testData2));
+        println("Day " + getDay() + " Q1: " + solveQ1(getInputLines()));
+
+        if (!testData1.isEmpty()) println("Test Q2: " + solveQ2(testData1));
+        if (!testData2.isEmpty()) println("Test Q2: " + solveQ2(testData2));
+        println("Day " + getDay() + " Q2: " + solveQ2(getInputLines()));
+    }
+
+    Object solveQ2(List<String> input) {
+        var result = 0L;
+
+        return result;
+    }
+
+    Object solveQ1(List<String> input) {
+        var result = 0L;
+
+        return result;
+    }
+
+}
+
