67ebf1af4f227024b1b0f1495346c645774825af
Debo <mail@deboder.nl>
2022-12-14 17:17:25 +0100

    2021 day 14 small clean

--- src/main/java/aoc/y2022/Day14.java
+++ src/main/java/aoc/y2022/Day14.java
@@ -24,11 +24,8 @@ public class Day14 extends AU {
 
     public static void main(String[] args) {
         if (!testData1.get().isEmpty()) new Day14(testData1.get(), true);
-        if (!testData2.get().isEmpty()) new Day14(testData2.get(), true);
         new Day14(null, true);
-        if (true) return;
         if (!testData1.get().isEmpty()) new Day14(testData1.get(), false);
-        if (!testData2.get().isEmpty()) new Day14(testData2.get(), false);
         new Day14(null, false);
     }
 
@@ -37,17 +34,7 @@ public class Day14 extends AU {
             503,4 -> 502,4 -> 502,9 -> 494,9
             """.lines().collect(toList());
 
-    static Supplier<List<String>> testData2 = () -> """
-            """.lines().collect(toList());
-
     Object solveQ2(List<String> input) {
-        var result = 0L;
-        //DAY2 !!
-        return result;
-    }
-
-    Object solveQ1(List<String> input) {
-        var starttime = System.currentTimeMillis();
         var result = 0L;
 
         var set = new HashSet<V2>();
@@ -55,21 +42,19 @@ public class Day14 extends AU {
         for (var line : input) {
             var split = line.split(" -> ");
             var last = toInts(split[0]);
-            var lastC = last[0];
-            var lastR = last[1];
-            var lastV = V2.of(lastR, lastC);
+            var lastV = V2.of(last[1], last[0]);
 
             set.add(lastV);
             for (var part : split) {
-                var nextints =toInts(part);
+                var nextints = toInts(part);
                 var nextV = V2.of(nextints[1], nextints[0]);
 
                 if (nextV.equals(lastV)) continue;
 
 
-                int minr = Math.min(lastV.row(), nextV.row());
+                int minr = min(lastV.row(), nextV.row());
                 int maxr = Math.max(lastV.row(), nextV.row());
-                int minc = Math.min(lastV.col(), nextV.col());
+                int minc = min(lastV.col(), nextV.col());
                 int maxc = Math.max(lastV.col(), nextV.col());
 
                 for (int r = minr; r <= maxr; r++) {
@@ -78,81 +63,88 @@ public class Day14 extends AU {
                     }
                 }
                 lastV = nextV;
-
             }
         }
 
-        var max = set.stream().mapToInt(v -> v.row()).max().orElse(0) + 2;
+        var max = set.stream()
+                .mapToInt(V2::row)
+                .max()
+                .orElse(0) + 2;
 
-        var maxrowa = V2.of(max, -1000);
-        var maxrowb = V2.of(max, 1000);
-        int minr = Math.min(maxrowa.row(), maxrowb.row());
-        int maxr = Math.max(maxrowa.row(), maxrowb.row());
-        int minc = Math.min(maxrowa.col(), maxrowb.col());
-        int maxc = Math.max(maxrowa.col(), maxrowb.col());
-
-
-        for (int r = minr; r <= maxr; r++) {
-            for (int c = minc; c <= maxc; c++) {
-                set.add(V2.of(r, c));
-            }
-        }
-//        printGrid(set);
+        for (int c = -1000; c <= 1000; c++) set.add(V2.of(max, c));
 
         while (true) {
-
             var sand = new V2(0, 500);
-            if (set.contains(sand)) {
-                System.out.println("time: " + (System.currentTimeMillis() - starttime));
-                return result;
-            }
+            if (set.contains(sand)) return result;
             boolean moved = true;
             result++;
             while (moved) {
-//                if (Math.abs(sand.row()) >= 1000) {
-//                    return result;
-//                }
-                moved = false;
                 var down = sand.up();
                 if (!set.contains(down)) {
                     sand = down;
-                    moved = true;
-
-                    continue;
-                }
-                var left = down.left();
-                if (!set.contains(left)) {
-                    sand = left;
-                    moved = true;
-                    continue;
-                }
-                var right = down.right();
-                if (!set.contains(right)) {
-                    sand = right;
-                    moved = true;
-                    continue;
+                } else if (!set.contains(down.left())) {
+                    sand = down.left();
+                } else if (!set.contains(down.right())) {
+                    sand = down.right();
+                } else {
+                    moved = false;
                 }
             }
             set.add(sand);
-
-
         }
-
-
-
-//        return result;
     }
 
-    private static void printGrid(HashSet<V2> visited) {
-        var minr = visited.stream().mapToInt(V2::row).min().orElse(0);
-        var minc = visited.stream().mapToInt(V2::col).min().orElse(0);
-        var maxr = visited.stream().mapToInt(V2::row).max().orElse(0);
-        var maxc = visited.stream().mapToInt(V2::col).max().orElse(0);
+    Object solveQ1(List<String> input) {
+        var result = 0L;
 
-        boolean[][] grid = new boolean[maxr + 1 - minr][maxc + 1 - minc];
+        var set = new HashSet<V2>();
 
-        for (var v : visited) grid[v.row() - minr][v.col() - minc] = true;
-        println(grid);
+        for (var line : input) {
+            var split = line.split(" -> ");
+            var last = toInts(split[0]);
+            var lastV = V2.of(last[1], last[0]);
+
+            set.add(lastV);
+            for (var part : split) {
+                var nextints = toInts(part);
+                var nextV = V2.of(nextints[1], nextints[0]);
+
+                if (nextV.equals(lastV)) continue;
+
+
+                int minr = min(lastV.row(), nextV.row());
+                int maxr = Math.max(lastV.row(), nextV.row());
+                int minc = min(lastV.col(), nextV.col());
+                int maxc = Math.max(lastV.col(), nextV.col());
+
+                for (int r = minr; r <= maxr; r++) {
+                    for (int c = minc; c <= maxc; c++) {
+                        set.add(V2.of(r, c));
+                    }
+                }
+                lastV = nextV;
+            }
+        }
+
+        while (true) {
+            var sand = new V2(0, 500);
+            boolean moved = true;
+            result++;
+            while (moved) {
+                if (sand.row() > 1000) return result -1;
+                var down = sand.up();
+                if (!set.contains(down)) {
+                    sand = down;
+                } else if (!set.contains(down.left())) {
+                    sand = down.left();
+                } else if (!set.contains(down.right())) {
+                    sand = down.right();
+                } else {
+                    moved = false;
+                }
+            }
+            set.add(sand);
+        }
     }
 }
 
